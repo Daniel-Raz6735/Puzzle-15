4c804ac90039ba47ae2e45dff8b6ec7eb8f6cdcb
Shlomo Carmi <shlomow6@gmail.com>
2021-03-14 22:25:54 +0200

    added necessary classes

--- app/src/main/java/com/danielr_shlomoc/ex1/GameActivity.java
+++ app/src/main/java/com/danielr_shlomoc/ex1/GameActivity.java
@@ -3,14 +3,12 @@ package com.danielr_shlomoc.ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class GameActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        Toast.makeText(this, "bla bla", Toast.LENGTH_SHORT).show();
+        setContentView(R.layout.activity_game);
     }
 }
---
+++ app/src/main/java/com/danielr_shlomoc/ex1/GameBoard.java
@@ -0,0 +1,4 @@
+package com.danielr_shlomoc.ex1;
+
+public class GameBoard {
+}
