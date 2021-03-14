b00ce038585f51b2449045d44851b7347a5bb316
Daniel-Raz <dany6735@gmail.com>
2021-03-15 01:27:19 +0200

    added Main activity

--- app/src/main/java/com/danielr_shlomoc/ex1/MainActivity.java
+++ app/src/main/java/com/danielr_shlomoc/ex1/MainActivity.java
@@ -2,15 +2,49 @@ package com.danielr_shlomoc.ex1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.EventListener;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "bla bla", Toast.LENGTH_SHORT).show();
 
+
+        Button startPlay = findViewById(R.id.btnPlayID);
+
+        startPlay.setOnClickListener(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+
+        MenuItem menuItem1 = menu.add("About");
+        MenuItem menuItem2 = menu.add("Exit");
+        return true;
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btnPlayID:
+                Intent gameActivity = new Intent(this, GameActivity.class);
+                startActivity(gameActivity);
+                break;
+
+        }
     }
 }
