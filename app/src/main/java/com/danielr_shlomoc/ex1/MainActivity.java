77f986c60581c0ae46dd4bafd69479dfd802a366
Shlomo Carmi <shlomow6@gmail.com>
2021-03-14 21:53:57 +0200

    hello APP

--- app/src/main/java/com/danielr_shlomoc/ex1/MainActivity.java
+++ app/src/main/java/com/danielr_shlomoc/ex1/MainActivity.java
@@ -3,6 +3,7 @@ package com.danielr_shlomoc.ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "bla bla", Toast.LENGTH_SHORT).show();
     }
 }
