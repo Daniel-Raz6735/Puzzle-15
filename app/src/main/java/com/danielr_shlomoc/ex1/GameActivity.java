18862b8364d2c4d5586c0fd99f80f278c3ba6dd4
Shlomo Carmi <shlomow6@gmail.com>
2021-03-18 12:44:47 +0200

    final working version

--- app/src/main/java/com/danielr_shlomoc/ex1/GameActivity.java
+++ app/src/main/java/com/danielr_shlomoc/ex1/GameActivity.java
@@ -4,7 +4,6 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,38 +25,34 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private int time;
     private boolean playing;
     private boolean alive;
+    private boolean game_over;
+    private Thread time_thread;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        moves_counter = findViewById(R.id.move_counter);
-        time_counter = findViewById(R.id.time_counter);
-        restart = findViewById(R.id.btn_re_start);
-        restart.setOnClickListener(this);
-        sp = getSharedPreferences("MyPref" , Context.MODE_PRIVATE);
-        playing = sp.getBoolean("play",false);
+        sp = getSharedPreferences("MyPref", Context.MODE_PRIVATE);
+        playing = sp.getBoolean("play", false);
         play_location = 0;
+        time_thread = null;
         initialize_btns();
         restart_game(null);
         moves = 0;
         time = 0;
-
-
-
-
-
+        game_over = false;
 
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        load_game();
-        start_timer();
+        if (!game_over)
+            start_timer();
         // get last state of media Player and play if last time was on.
-        if(playing){
-            player =  MediaPlayer.create(this, R.raw.janji);
+        if (playing) {
+            player = MediaPlayer.create(this, R.raw.janji);
             player.setLooping(true);
             player.seekTo(play_location);
             player.start();
@@ -68,9 +63,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onPause() {
         super.onPause();
-        alive=false;
+        alive = false;
         stopPlaying();
-        save_game();
     }
 
     @Override
@@ -80,74 +74,78 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         sp.edit().apply();
     }
 
+    @Override
+    public void onClick(View v) {
+
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (v.getId() == btns[i][j].getId()) {
+                    if (play_move(i, j))
+                        test_won();
+                }
+            }
+        }
+        if (v.getId() == restart.getId()) {
+            restart_game(null);
+            game_over = false;
+
+        }
+    }
+
+    private void stopPlaying() {
+        //stop the music from playing
+        if (player != null) {
+            play_location = player.getCurrentPosition();
+            player.stop();
+            player.release();
+            player = null;
+        }
+    }
+
     public void remove_color(TextView t) {
+        //remove the color from a button
         t.setBackgroundResource(R.drawable.border_black_white_bcg);
         t.setTextColor(getResources().getColor(R.color.transparent));
         t.setText("");
         t.setClickable(false);
     }
 
-    public void save_game() {
-        SharedPreferences.Editor editor = sp.edit();
-        //save board
-        int[][] board = game.getBoard();
-        editor.putBoolean("board",true);
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                editor.putInt("btn" + i + j, board[i][j]);
-                Log.i("test_changes", "btn" + i + j);
-            }
-        }
-
-        editor.apply();
-    }
-
-    public void load_game() {
-        SharedPreferences.Editor editor = sp.edit();
-        //save board
-        if(sp.getBoolean("board" ,false)){
-
-
-        int[][] board = game.getBoard();
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                int num = sp.getInt("btn" + i + j,-1);
-
-
-                editor.putInt("btn" + i + j, board[i][j]);
-                Log.i("test_changes", "btn" + i + j);
-            }
-        }
-        }
-
-        editor.apply();
-    }
-
     public void start_timer() {
-        alive=true;
-        new Thread(new Runnable() {
-            public void run() {
-                while(alive) {
-                    try {
-                        Thread.sleep(1000);
-                        time++;
-                        int minutes = time / 60;
-                        int seconds = time % 60;
-                        runOnUiThread(new Runnable() {
-                            public void run() {
-                                time_counter.setText("Time: " + String.format("%02d", minutes) + ":" + String.format("%02d", seconds));
-                            }
-                        });
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
+        //sets a timer in the app to show the user the amount of time he is playing
+        if (time_thread == null) {
+            //if timer is already set
+            alive = true;
+            time_thread = new Thread(new Runnable() {
+                public void run() {
+                    while (alive) {
+                        try {
+                            Thread.sleep(1000);
+                            time++;
+                            int minutes = time / 60;
+                            int seconds = time % 60;
+                            runOnUiThread(new Runnable() {
+                                public void run() {
+                                    time_counter.setText("Time: " + String.format("%02d", minutes) + ":" + String.format("%02d", seconds));
+                                }
+                            });
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
                     }
+                    time_thread = null;
                 }
-            }
-        }).start();
+            });
+            time_thread.start();
+        }
     }
 
     public void initialize_btns() {
-        //connects the buttons to the btns array and sets them the listener
+        //connects all of the buttons to the variables and sets them a listener
+        moves_counter = findViewById(R.id.move_counter);
+        time_counter = findViewById(R.id.time_counter);
+        restart = findViewById(R.id.btn_re_start);
+        restart.setOnClickListener(this);
+        // connect the board
         btns = new TextView[SIZE][SIZE];
         TextView[] row = btns[0];
         row[0] = findViewById(R.id.txt_1);
@@ -177,6 +175,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void color_view(TextView t, int num) {
+        //color the background of a button to be a colored button with the number appearing in it
         t.setBackgroundResource(R.drawable.btn_bcg);
         t.setTextColor(getResources().getColor(R.color.btn_txt_clr));
         t.setText(num + "");
@@ -184,22 +183,27 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void update_moves() {
-        moves ++;
-        moves_counter.setText("Moves: "+String.format("%04d", moves));
+        //add moves to the counter
+        moves++;
+        moves_counter.setText("Moves: " + String.format("%04d", moves));
     }
 
-    public void test_won(){
-        if(game.checkForWin()){
-            for (int i = 0; i < SIZE; i++) {
+    public void test_won() {
+        //test to see if the game is finished
+        if (game.checkForWin()) {
+            game_over = true;
+            for (int i = 0; i < SIZE; i++) {//make all buttons un clickable
                 for (int j = 0; j < SIZE; j++) {
                     btns[i][j].setClickable(false);
                 }
             }
-            Toast.makeText(this,"Game Over - Puzzle Solved!",Toast.LENGTH_LONG).show();
-            alive=false;
+            Toast.makeText(this, "Game Over - Puzzle Solved!", Toast.LENGTH_LONG).show();
+            alive = false;
         }
     }
+
     public boolean play_move(int x, int y) {
+        // ask the game class if the move is legal if so play the move otherwise return null
         int[] res = game.play_move(x, y);
         if (res == null)
             return false;
@@ -213,6 +217,7 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     public void color_board(int[][] new_board) {
+        //color all buttons on the board except for the blank tile that should be blank
         int run = new_board.length;
         if (run > SIZE)
             run = SIZE;
@@ -229,40 +234,20 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void restart_game(int [][] board) {
-
-        if(board == null){
+    public void restart_game(int[][] board) {
+        //restart the game and play start a new game if none is provided
+        if (board == null) {
             game = new GameBoard();
             board = game.getBoard();
         }
+        start_timer();
         color_board(board);
+        game_over = false;
         moves = 0;
         time = 0;
         moves_counter.setText("Moves: 0000");
-        time_counter.setText("Time: 0000");
+        time_counter.setText("Time: 00:00");
     }
 
 
-    @Override
-    public void onClick(View v) {
-
-        for (int i = 0; i < SIZE; i++) {
-            for (int j = 0; j < SIZE; j++) {
-                if (v.getId() == btns[i][j].getId()) {
-                    if (play_move(i, j))
-                        test_won();
-                }
-            }
-        }
-        if (v.getId() == restart.getId())
-            restart_game(null);
-    }
-        private void stopPlaying() {
-            if (player != null) {
-                play_location = player.getCurrentPosition();
-                player.stop();
-                player.release();
-                player = null;
-            }
-        }
 }
--- app/src/main/java/com/danielr_shlomoc/ex1/GameBoard.java
+++ app/src/main/java/com/danielr_shlomoc/ex1/GameBoard.java
@@ -1,144 +1,117 @@
 package com.danielr_shlomoc.ex1;
 
-import android.util.Size;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
-//import java.util.*;
 
 public class GameBoard {
-    private final int SIZE = 4;
-    private int[][] board;
-    private final int[][] goalState = {{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,0}};
-    public int invCount =0;
-    public int x =0;
+    private final int SIZE = 4; //contains the size of the game
+    private final int[][] goalState = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 0}}; //expected game result
+    private int[][] board; // contains the game board
+    private int[] x_location; // an array with the location of the blank tile location
 
 
-
-
-    public GameBoard(){
+    public GameBoard() {
         board = new int[SIZE][SIZE];
-
         randomBoard();
     }
 
-    // This function random a solvable board.
     public void randomBoard() {
+    // This function creates a random solvable board.
         List<Integer> list;
-        do{
-
-            list = new ArrayList<Integer>();
-
-            for(int i =0 ;i<=15;i++)
+        do {
+            list = new ArrayList<>();
+            for (int i = 0; i <= 15; i++)
                 list.add(i);
-
             Collections.shuffle(list);
 
             int i = 0;
-
-            for(int row = 0 ;row<this.board.length;row++)
-                for(int col =0; col< this.board.length;col++) {
+            for (int row = 0; row < this.board.length; row++)
+                for (int col = 0; col < this.board.length; col++) {
                     this.board[row][col] = list.get(i);
+                    if (list.get(i) == 0)
+                        x_location = new int[]{row, col};
                     i++;
                 }
-        }while(!isSolvable(this.board, list));
-
-
+        } while (!isSolvable(list));
 
     }
 
-    public int [] play_move(int x, int y){
-        // i,j indexes of 0 in the board
-        int i = findXPosition()[1], j = findXPosition()[2];
+    public int[] play_move(int row, int col) {
+        /*
+        test if the given indexes are a valid move in the game
+        returns an array { value of the tile moved, new location row, new location column}
+        */
+
+        int i = x_location[0], j = x_location[1]; // i,j indexes of the blank tile in the board
         boolean flag = false;
 
-
-        if(x == i) {
-
-            int diff = y -j;
-
+        if (row == i) {
+            int diff = col - j;
             // move right or left
-            if(diff == -1 || diff == 1)
-                flag  = true;
-
-        }
-
-        if( j == y) {
-
-            int diff = x -i;
-
-            // move up or down
-            if(diff == -1 || diff ==1 )
+            if (diff == -1 || diff == 1)
                 flag = true;
         }
 
-        if(!flag)
+        if (j == col) {
+            int diff = row - i;
+            // move up or down
+            if (diff == -1 || diff == 1)
+                flag = true;
+        }
+
+        if (!flag) //illegal move
             return null;
 
-        board[i][j] = board[x][y];
-        board[x][y] = 0;
-
-        return new int[]{i,j,board[i][j]};
+        board[i][j] = board[row][col];
+        board[row][col] = 0;
+        this.x_location = new int []{row,col}; //save the new location of the empty tile
+        return new int[]{i, j, board[i][j]};
 
     }
 
     public boolean checkForWin() {
+        //test the board and see if it is complete
         return Arrays.deepEquals(this.board, this.goalState);
     }
 
-    private int getInvCount(List<Integer> list)
-    {
-        final int  N = this.SIZE;
+    private int getInvCount(List<Integer> list) {
+        final int N = this.SIZE;
         int inv_count = 0;
-        for (int i = 0; i < N * N - 1; i++)
-        {
-            for (int j = i + 1; j < N * N; j++)
-            {
+        for (int i = 0; i < N * N - 1; i++) {
+            for (int j = i + 1; j < N * N; j++) {
                 // count pairs(i, j) such that i appears
                 // before j, but i > j.
-                if (list.get(i) > list.get(j)&&list.get(i)>0&&list.get(j)>0)
+                if (list.get(i) > list.get(j) && list.get(i) > 0 && list.get(j) > 0)
                     inv_count++;
             }
         }
         return inv_count;
     }
 
-    // find Position of blank from bottom
-    public int[] findXPosition()
-    {
-        final int N = this.SIZE;
-        // start from bottom-right corner of matrix
-        for (int i = N - 1; i >= 0; i--)
-            for (int j = N - 1; j >= 0; j--)
-                if (this.board[i][j] == 0)
-                    return new int[] {N - i,i,j};
-
-        return null;
-    }
 
     // This function returns true if given
     // instance of N*N - 1 puzzle is solvable
-    private boolean isSolvable(int puzzle[][], List<Integer> list)
-    {
+    private boolean isSolvable(List<Integer> list) {
+        /*
+        tests a board to see if it solvable. a board will be considered solvable if it has
+         or 1.even amount of inversions and the blank tile is an odd number of rows from the bottom
+         (last row is considered one)
+         or 2.odd amount of inversions and the blank tile is an even number of rows from the bottom*/
 
         // Count inversions in given puzzle
         int invCount = getInvCount(list);
-        int pos = findXPosition()[0];
-        this.invCount=invCount;
-        this.x=pos;
-        if (pos%2 != 0)
-            return !(invCount%2 != 0);
-        else
-            return invCount%2 != 0;
+        int lines_from_bottom = this.SIZE - x_location[0];
+        return lines_from_bottom % 2 == 0 ^ invCount % 2 == 0;//XOR
 
     }
 
-    public int [][] getBoard(){
-        int [][] res = new int[SIZE][SIZE];
-        for(int i=0; i<SIZE;i++)
+    public int[][] getBoard() {
+        //board getter
+        int[][] res = new int[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++)
             System.arraycopy(board[i], 0, res[i], 0, SIZE);
         return res;
     }
